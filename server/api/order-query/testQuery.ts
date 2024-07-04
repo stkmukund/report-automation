971bd494c3dfa2b6fae5a57c56415642ab683634
stkmukund <164354566+stkmukund@users.noreply.github.com>
2024-07-05 00:12:13 +0530

    testing

--- server/api/order-query/testQuery.ts
+++ server/api/order-query/testQuery.ts
@@ -1,7 +1,7 @@
 
 
 export default defineEventHandler( async (event)=> {
-    let response = await fetch(`https://api.checkoutchamp.com/order/query/?loginId=revboostapirs.nymbus&password=RSsfFrR2nN5PcC6L1pSRs&campaignId=39&startDate=06/01/2024&endDate=07/01/2024&resultsPerPage=200&orderStatus=REFUNDED`);
+    let response = await fetch(`https://api.checkoutchamp.com/order/query/?loginId=revboostapirs.nymbus&password=RSsfFrR2nN5PcC6L1pSRs&campaignId=39&startDate=06/01/2024&endDate=07/01/2024&resultsPerPage=1&orderStatus=REFUNDED`);
 
     return await response.json();
 })
