c81337bb4f380a0456a292415747933481722438
stkmukund <164354566+stkmukund@users.noreply.github.com>
2024-07-05 14:17:28 +0530

    test API

--- server/api/order-query/testQuery.ts
+++ server/api/order-query/testQuery.ts
@@ -1,7 +1,7 @@
 
 
 export default defineEventHandler( async (event)=> {
-    let response = await fetch(`https://api.checkoutchamp.com/transactions/summary/?loginId=revboostapirs.nymbus&password=RSsfFrR2nN5PcC6L1pSRs&startDate=07/01/2024&endDate=07/05/2024&reportType=product&productId=1&campaignCategory=12`);
+    let response = await fetch(`https://api.checkoutchamp.com/transactions/summary/?loginId=revboostapirs.nymbus&password=RSsfFrR2nN5PcC6L1pSRs&startDate=07/01/2024&endDate=07/05/2024&reportType=product&productId=ONE_TIME&campaignCategory=12`);
 
     return await response.json();
 })
