2063d42cd623633de38ef42a9a3ed29e974a32a6
stkmukund <164354566+stkmukund@users.noreply.github.com>
2024-07-06 15:30:45 +0530

    ignore

--- server/api/order-query/testQuery.ts
+++ server/api/order-query/testQuery.ts
@@ -2,7 +2,7 @@
 
 export default defineEventHandler( async (event)=> {
     // let response = await fetch(`https://api.checkoutchamp.com/transactions/summary/?loginId=revboostapirs.nymbus&password=RSsfFrR2nN5PcC6L1pSRs&startDate=07/01/2024&endDate=07/05/2024&reportType=campaign&campaignCategory=12&dateRangeType=chargebackDate`);
-    let response = await fetch(`https://api.checkoutchamp.com/order/query/?loginId=revboostapirs.nymbus&password=RSsfFrR2nN5PcC6L1pSRs&campaignId=61,47,1,68,9,6,67,69,70&startDate=07/01/2024&endDate=07/05/2024&resultsPerPage=10&orderStatus=COMPLETE&orderType=NEW_SALE&campaignProductId=7`);
+    let response = await fetch(`https://api.checkoutchamp.com/order/query/?loginId=revboostapirs.nymbus&password=RSsfFrR2nN5PcC6L1pSRs&campaignId=61,47,1,68,9,6,67,69,70&startDate=07/01/2024&endDate=07/06/2024&resultsPerPage=10&orderStatus=COMPLETE&orderType=NEW_SALE&campaignProductId=7`);
 
     return await response.json();
 })
